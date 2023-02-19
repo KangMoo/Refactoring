a5336a113ff3eb959dca4e22a26d2b6bae49651d
hkm0629 <hkm0629@uangel.com>
2023-02-19 11:24:07 +0900

    07. Refactor Tangled Change

---
+++ src/main/java/me/whiteship/refactoring/_07_divergent_change/_24_split_phase/PriceData.java
@@ -0,0 +1,7 @@
+package me.whiteship.refactoring._07_divergent_change._24_split_phase;
+
+/**
+ * @author kangmoo Heo
+ */
+public record PriceData(double basePrice, double discount, int quantity) {
+}
