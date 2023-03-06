634d3f74018b7d065bbbed922470a80deee437bb
hkm0629 <hkm0629@uangel.com>
2023-03-07 00:03:53 +0900

    23. Refactor Refused bequest

--- src/main/java/me/whiteship/refactoring/_23_refused_bequest/Quota.java
+++ src/main/java/me/whiteship/refactoring/_23_refused_bequest/Quota.java
@@ -1,4 +1,10 @@
 package me.whiteship.refactoring._23_refused_bequest;
 
 public class Quota {
+
+    protected Quota quota;
+
+    protected Quota getQuota() {
+        return new Quota();
+    }
 }
