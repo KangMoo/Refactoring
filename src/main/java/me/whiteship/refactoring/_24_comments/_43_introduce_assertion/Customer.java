b8aa4dc4ddb6414f4c3a25a0e55108a455a02cfb
hkm0629 <hkm0629@uangel.com>
2023-03-07 20:34:49 +0900

    24. Refactor Comments

--- src/main/java/me/whiteship/refactoring/_24_comments/_43_introduce_assertion/Customer.java
+++ src/main/java/me/whiteship/refactoring/_24_comments/_43_introduce_assertion/Customer.java
@@ -13,6 +13,7 @@ public class Customer {
     }
 
     public void setDiscountRate(Double discountRate) {
+        assert discountRate != null && discountRate > 0; // 주석보다 assert 사용하는 것이 더 좋음. (추후 배포 시에는 예외처리 필요)
         this.discountRate = discountRate;
     }
 }
